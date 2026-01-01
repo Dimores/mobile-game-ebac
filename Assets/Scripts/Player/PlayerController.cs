b55411cf04592fb6cd42c6aac40da0e32d52a288
Diego Morelo <xxkirito31@gmail.com>
2026-01-01 16:56:09 -0300

    feat: add class 4 files.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,17 +4,43 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    #region PUBLICS
+    [Header("Lerp")]
+    public Transform target;
+    public float lerpSpeed = 0.1f;
+
     public float speed = 1f;
 
-    // Start is called before the first frame update
+    public string enemyTag = "Enemy";
+    #endregion
+
+    #region PRIVATES
+    private bool _canRun;
+    private Vector3 _pos;
+    #endregion
+
     void Start()
     {
-        
+        _canRun = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.Translate(transform.forward * speed * Time.deltaTime); 
+        if (!_canRun) return;
+
+        _pos = target.position;
+        _pos.y = transform.position.y;
+        _pos.z = transform.position.z;
+
+        transform.Translate(transform.forward * speed * Time.deltaTime);
+        transform.position = Vector3.Lerp(transform.position, _pos, lerpSpeed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == enemyTag)
+        {
+            _canRun = false;
+        }
     }
 }
