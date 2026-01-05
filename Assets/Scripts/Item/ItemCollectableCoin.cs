69b8878c4f42efcc0c7d0458d88999117c8efa70
Diego Morelo <xxkirito31@gmail.com>
2026-01-05 19:07:41 -0300

    feat: add power ups.

--- Assets/Scripts/Item/ItemCollectableCoin.cs
+++ Assets/Scripts/Item/ItemCollectableCoin.cs
@@ -1,26 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 //using DG.Tweening;
 //using Orby.Managers; // Importa o DOTween
 
 public class ItemCollectableCoin : ItemCollectableBase
 {
-    [Header("Animation Config")]
-    public float rotationSpeed = 1f; 
-    public float floatHeight = 0.2f; 
-    public float floatDuration = 1f;
+    [Header("Coin Specific Config")]
+    public Collider coinCollider;
+    public bool collect = false;
+    public float lerp = 5f;
+    public float minDistance = 1f;
 
-    [Header("VFX Collider")]
-    public Transform vfxCollider;
+    //[Header("Animation Config")]
+    //public float rotationSpeed = 1f; 
+    //public float floatHeight = 0.2f; 
+    //public float floatDuration = 1f;
+
+
+    //[Header("VFX Collider")]
+    //public Transform vfxCollider;
+
 
     //private Tween _rotationTween;
     //private Tween _floatTween;
 
+
     private void Start()
+
     {
-        AnimateCoin(); 
+        //AnimateCoin(); 
+        //CoinsAnimationManager.Instance.RegisterCoin(this);
     }
+
+    private void Update()
+    {
+        if (collect)
+        {
+            transform.position = Vector3.Lerp(transform.position,
+                PlayerController.Instance.transform.position, lerp * Time.deltaTime);
+
+            if (Vector3.Distance(transform.position, PlayerController.Instance.transform.position) < minDistance)
+            {
+                HideItens();
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void HideItens()
+    {
+        GetComponentInChildren<MeshRenderer>().enabled = false;
+    }
+
 
     private void AnimateCoin()
     {
@@ -32,13 +65,17 @@ public class ItemCollectableCoin : ItemCollectableBase
     protected override void OnCollect()
     {
         base.OnCollect();
-        ItemManager.Instance.AddCoins();
+        coinCollider.enabled = false;
+        collect = true;
+        //PlayerController.Instance.Bounce();
+
+        //ItemManager.Instance.AddCoins();
         //VFXManager.Instance.PlayVFXByTypeWithCollision(VFXManager.VFXType.COIN, this.transform.position, 
         //    null, vfxCollider);
     }
 
-    private void OnDestroy()
+    protected override void Collect()
     {
-        //_floatTween?.Kill();
+        OnCollect();
     }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,29 +1,47 @@
+using DG.Tweening;
+using Orby.Core.Singleton;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : Singleton<PlayerController>
 {
     #region PUBLICS
     [Header("Lerp")]
     public Transform target;
     public float lerpSpeed = 0.1f;
-
     public float speed = 1f;
 
+    [Header("Tags")]
     public string enemyTag = "Enemy";
     public string endLineTag = "EndLine";
 
+    [Header("UI")]
     public GameObject endScreen;
+
+    [Header("TextMeshPro")]
+    public TextMeshPro uiTextPowerUp;
+
+    [Header("Coin Setup")]
+    public GameObject coinCollector;
     #endregion
 
     #region PRIVATES
     private bool _canRun;
     private Vector3 _pos;
+
+    private Vector3 _startPosition;
+
+    private float _currentSpeed;
+
+    private bool _invencible = false;
     #endregion
 
     void Start()
     {
+        _startPosition = transform.position;
+        ResetSpeed();
     }
 
     void Update()
@@ -34,7 +52,7 @@ public class PlayerController : MonoBehaviour
         _pos.y = transform.position.y;
         _pos.z = transform.position.z;
 
-        transform.Translate(transform.forward * speed * Time.deltaTime);
+        transform.Translate(transform.forward * _currentSpeed * Time.deltaTime);
         transform.position = Vector3.Lerp(transform.position, _pos, lerpSpeed * Time.deltaTime);
     }
 
@@ -51,17 +69,63 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.transform.tag == enemyTag)
+        if (collision.transform.tag == enemyTag)
         {
-            EndGame();
+            if (!_invencible) EndGame();
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.transform.tag == endLineTag)
+        if (other.transform.tag == endLineTag)
         {
-            EndGame();
+            if (!_invencible) EndGame();
         }
     }
+
+    #region POWER UPS
+    public void SetPowerUpText(string s)
+    {
+        uiTextPowerUp.text = s;
+    }
+    public void PowerUpSpeedUp(float f)
+    {
+        _currentSpeed = f;
+    }
+    public void ResetSpeed()
+    {
+        _currentSpeed = speed;
+    }
+
+    public void SetInvencible(bool b = true)
+    {
+        _invencible = b;
+    }
+
+    public void ChangeHeight(float amount, float duration, float animationDuration, Ease ease)
+    {
+        //var p = transform.position;
+        //p.y = _startPosition.y + amount;
+        //transform.position = p;
+
+        transform.DOMoveY(_startPosition.y + amount,
+            animationDuration).SetEase(ease);//.OnComplete(ResetHeight);
+
+        Invoke(nameof(ResetHeight), duration);
+    }
+
+    public void ResetHeight()
+    {
+        //var p = transform.position;
+        //p.y = _startPosition.y;
+        //transform.position = p;
+
+        transform.DOMoveY(_startPosition.y, .1f);
+    }
+
+    public void ChangeCoinCollectorSize(float amount)
+    {
+        coinCollector.transform.localScale = Vector3.one * amount;
+    }
+    #endregion
 }
